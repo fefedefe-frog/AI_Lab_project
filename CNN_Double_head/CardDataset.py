8b6e49f04287ad336c65ca5dcc986fa719059e7d
Doct0rDuc <federicoiannucci2@gmail.com>
2025-05-29 00:36:14 +0200

    moved the dataset class definition in a separated file for better comprehension

---
+++ CNN_Double_head/CardDataset.py
@@ -0,0 +1,48 @@
+import cv2
+import pandas as pd
+
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+class CardDataset(Dataset):
+    def __init__(self, dataset_path, csv_file_path: str, transform: transforms) -> None:
+
+        # Carico il csv usando pandas
+        self.dataset_path= dataset_path
+        self.csv_data = pd.read_csv(csv_file_path)
+        self.transforms = transform
+
+        # Definisco le classi per seme e numeri
+        self.classi_seme: tuple= ("cuori", "quadri", "fiori", "picche")
+        self.classi_numero: tuple= ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "K", "Q", "J")
+
+        # Converto le etichette testuali in indici numerici usabili dalla rete neurale
+        self.seme_to_idx = {cls: idx for idx, cls in enumerate(self.classi_seme)}
+        self.numero_to_idx = {cls: idx for idx, cls in enumerate(self.classi_numero)}
+
+    def __len__(self) -> int:
+        return len(self.csv_data)
+
+    def __getitem__(self, idx) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        riga = self.csv_data.iloc[idx]
+
+        image_path = f"{self.dataset_path}/{str(riga["image_path"])}"
+        seme = self.seme_to_idx[riga["seme"]]
+        numero = self.numero_to_idx[riga["numero"]]
+
+        image = cv2.imread(image_path)
+        if image is None:
+            raise FileNotFoundError(f"{image_path} not found")
+
+        if self.transforms:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            out_img = self.transforms(image)
+        else:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            image = cv2.resize(image, (240, 180))
+            image = image.astype(np.float32) / 255.0
+            image = np.transpose(image, (2, 0, 1))
+            out_img = torch.tensor(image, dtype=torch.float32)
+
+        return out_img, torch.tensor(seme, dtype=torch.long), torch.tensor(numero, dtype=torch.long)
