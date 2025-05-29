efe5d098d1f2054bd437db957883353b1dd835a0
Doct0rDuc <federicoiannucci2@gmail.com>
2025-05-29 04:00:15 +0200

    minor changes

--- YOLO_dataset_maker/main.py
+++ YOLO_dataset_maker/main.py
@@ -1,5 +1,5 @@
 import argparse
-import YOLO_dataset_maker.generate_dataset as rot
+import YOLO_dataset_maker.generate_dataset as generator
 
 parser= argparse.ArgumentParser(description="YOLO dataset maker, this script automatically generate the images and labels for yolo")
 parser.add_argument("image_num", type=int, help="how many images to generate")
@@ -10,4 +10,4 @@ parser.add_argument("-r", "--rotation-on", help="enable random rotation of the g
 if __name__ == "__main__":
     args= parser.parse_args()
 
-    rot.generate(args.rotation_on, args.image_num, args.output_path)
+    generator.generate(args.rotation_on, args.image_num, args.output_path)
