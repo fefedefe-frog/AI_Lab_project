263c862e52d07becf82332954b2e33f63ea87982
GiovanniCiarra <jovy.jr22@gmail.com>
2025-05-29 16:20:34 +0200

    piccole modifiche di funzionamento

--- blackjack_card_detector.py
+++ blackjack_card_detector.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 
 # === CONFIG ===
 YOLO_MODEL_PATH = "YOLO_cards_detector/runs/detect/yolo_finetuned_blackjack2/weights/best.pt"
-CNN_MODEL_PATH = "CNN_Double_head/cnn_allenata.pth"
-IMAGE_PATH = "test_images_complete_net/test4.png"
+CNN_MODEL_PATH = "CNN_Double_head/output_6/cnn_allenata_2.pth"
+IMAGE_PATH = "test_images_complete_net/test6.png"
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # === 1. Carica YOLO
