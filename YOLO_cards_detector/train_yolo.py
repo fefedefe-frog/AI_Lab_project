ddbde9c3556192c956ba997102c39077d98bfb01
Doct0rDuc <federicoiannucci2@gmail.com>
2025-05-29 20:22:57 +0200

    minor change

--- YOLO_cards_detector/train_yolo.py
+++ YOLO_cards_detector/train_yolo.py
@@ -16,7 +16,7 @@ parser.add_argument("data_path", type=str, help="path to the data.yaml file")
 parser.add_argument("epochs", type=int, help="how many epochs the model will train")
 parser.add_argument("-b", "--batch_size", type=int, help="the size of the batch")
 parser.add_argument("-i", "--img_sz", type=int, help="how yolo will resize the image from the dataset")
-parser.add_argument("-0", "--output_folder_name", type=str, help="name of the output folder")
+parser.add_argument("-o", "--output_folder_name", type=str, help="name of the output folder")
 
 
 if __name__ == "__main__":
